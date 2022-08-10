af77dfba766438adc97dd092371be4ebc6b7e287
caishuxiang <1057756098@qq.com>
2022-08-10 20:51:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+## 通过iframe打印网页局部内容
+
+## 使用方法
+```javascript
+import quickPrint from "@sxcai/quick-print";
+quickPrint(document.querySelector("#testPrint").innerHTML, {
+    direction: 'portrait', // auto landscape  打印方向
+});
+/** 第2个参数是配置项，可配置如下内容
+ {
+    direction = "portrait", // 方向 默认竖向打印 landscape：横向打印；auto：自动
+    style="", // 样式
+    iframeId = "printIframe", // iframe id
+    beforePrint // callback before print
+    } 
+ */
+```
---
+++ src/index.js
@@ -0,0 +1,39 @@
+/**
+ * 通过iframe打印网页局部内容
+ * @param {string} content 打印内容html 
+ * @param {object} options 配置对象
+ */
+export default function (content, options = {}) {
+    const {
+        direction = "portrait", // 方向 默认竖向打印 landscape：横向打印；auto：自动
+        style="", // 样式
+        iframeId = "printIframe", // iframe id
+        beforePrint // callback before print
+    } = options;
+
+    const iframe = document.createElement("iframe");
+    iframe.id = iframeId;
+    iframe.style.display = "none";
+
+    document.documentElement.appendChild(iframe);
+
+    const printWindow = iframe.contentWindow;
+
+    printWindow.document.write(`
+        <style>
+            body { -webkit-print-color-adjust: exact; }
+            @page { size: ${direction};}
+            ${style}
+        </style>
+        ${content}
+    `);
+
+    beforePrint && printWindow.addEventListener("beforeprint", beforeprint);
+
+    printWindow.focus();
+    printWindow.print();
+
+    printWindow.parent.document
+        .querySelectorAll(`#${iframeId}`)
+        .forEach(item => item.remove());
+}
